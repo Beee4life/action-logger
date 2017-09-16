e2afd6312c9676aabe3966940f3bc65bfe576dac
Beee <info@berryplasman.com>
2017-09-16 06:32:42 +0200

    Remove double button

--- al-dashboard.php
+++ al-dashboard.php
@@ -76,11 +76,6 @@
                         <?php } ?>
                         </tbody>
                     </table>
-                    <?php if ( current_user_can( 'manage_options' ) ) { ?>
-                        <input name="delete" type="submit" class="admin-button admin-button-small" value="<?php esc_html_e( 'Delete selected items', 'action-logger' ); ?>" />
-                    <?php } ?>
-                    </tbody>
-                </table>
                 <?php if ( current_user_can( 'manage_options' ) ) { ?>
                     <br />
                     <input name="delete" type="submit" class="admin-button admin-button-small" value="<?php esc_html_e( 'Delete selected items', 'action-logger' ); ?>" />
