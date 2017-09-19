52182fb4be4199330f24121ed0326e49ed4caa01
Beee <info@berryplasman.com>
2017-09-19 15:23:37 +0200

    Move capabilities form

--- al-misc.php
+++ al-misc.php
@@ -23,6 +23,30 @@
 
                 <?php echo al_admin_menu(); ?>
 
+                <form name="capabilities-form" action="" method="post">
+                    <input name="active_logs_nonce" type="hidden" value="<?php echo wp_create_nonce( 'active-logs-nonce' ); ?>"/>
+                    <h2><?php esc_html_e( 'Who can do what', 'action-logger' ); ?></h2>
+                    <p>
+                        <?php esc_html_e( 'Here you can select what capability a user needs to see the logs. The default setting is "manage_options" which belongs to administrator.', 'action-logger' ); ?>
+                        <?php esc_html_e( 'The reason why it\'s set per capability instead of per user is because two users with the same role can have different capabilities.', 'action-logger' ); ?>
+                    </p>
+                    <p>
+                        <?php
+                            $all_capabilities = get_role( 'administrator' )->capabilities;
+                            $logs_user_role   = get_option( 'al_log_user_role' );
+                            ksort( $all_capabilities );
+                        ?>
+                        <label for="select_cap" class="screen-reader-text"></label>
+                        <select name="select_cap" id="select_cap">
+                            <?php foreach ( $all_capabilities as $key => $value ) { ?>
+                                <option value="<?php echo $key; ?>"<?php echo ( $logs_user_role == $key ? ' selected' : '' ); ?>><?php echo $key; ?></option>';
+                            <?php } ?>
+                        </select>
+                    </p>
+                    <br />
+                    <input type="submit" class="admin-button admin-button-small" value="<?php esc_html_e( 'Save settings', 'action-logger' ); ?>" />
+                </form>
+
                 <h2><?php esc_html_e( 'Export data to CSV', 'action-logger' ); ?></h2>
                 <p><?php esc_html_e( 'By clicking this button you will trigger a download for a CSV (comma separated value) file.', 'action-logger' ); ?></p>
                 <form name="export-form" action="" method="post">
