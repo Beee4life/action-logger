3d6a47ef34629053e481c4c166d1537f03eb4683
Beee <info@berryplasman.com>
2017-11-29 20:54:22 +0100

    Change menu

--- al-admin-menu.php
+++ al-admin-menu.php
@@ -1,6 +1,11 @@
 <?php
 	function al_admin_menu() {
-        if ( current_user_can( get_option( 'al_log_user_role' ) ) ) {
-            return '<p><a href="' . site_url() . '/wp-admin/admin.php?page=action-logger">' . esc_html__( 'Logs', 'action-logger' ) . '</a> | <a href="' . site_url() . '/wp-admin/admin.php?page=al-log-actions">' . esc_html__( 'Log actions', 'action-logger' ) . '</a> | <a href="' . site_url() . '/wp-admin/admin.php?page=al-settings">' . esc_html__( 'Settings', 'action-logger' ) . '</a> | <a href="' . site_url() . '/wp-admin/admin.php?page=al-misc">' . esc_html__( 'Misc', 'action-logger' ) . '</a></p>';
-        }
-    }
+
+		$string = false;
+
+		if ( current_user_can( 'manage_options' ) ) {
+			$string = '<p><a href="' . site_url() . '/wp-admin/admin.php?page=action-logger">' . esc_html__( 'Logs', 'action-logger' ) . '</a> | <a href="' . site_url() . '/wp-admin/admin.php?page=al-log-actions">' . esc_html__( 'Log actions', 'action-logger' ) . '</a> | <a href="' . site_url() . '/wp-admin/admin.php?page=al-settings">' . esc_html__( 'Settings', 'action-logger' ) . '</a> | <a href="' . site_url() . '/wp-admin/admin.php?page=al-misc">' . esc_html__( 'Misc', 'action-logger' ) . '</a></p>';
+		}
+
+		return $string;
+	}
