d86f27c1a624f17a034fc6bc02754e737f32ffc1
Beee <info@berryplasman.com>
2018-02-02 03:43:45 +0100

    Improve purge

--- ActionLogger.php
+++ ActionLogger.php
@@ -254,9 +254,9 @@
 	         * Function which runs when cron job is triggered
 	         */
 	        public function al_cron_jobs() {
-		        $purge_logs_after = ( false != get_option( 'al_purge_logs' ) ) ? intval( get_option( 'al_purge_logs' ) ) : 30;
+		        $purge_logs_after = get_option( 'al_purge_logs', 0 );
 		        // only purge when it's not set to forever/0
-		        if ( 0 !== $purge_logs_after ) {
+		        if ( 0 != $purge_logs_after ) {
 			        $now_ts           = current_time( 'timestamp' );
 			        $purge_range      = $purge_logs_after * 24 * 60 * 60;
 			        $purge_date       = $now_ts - $purge_range;
