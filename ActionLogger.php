5d76311e42fa26472dfc261b30a5335a52907572
Beee <info@berryplasman.com>
2018-02-27 12:55:14 +0100

    Fix cron job

--- ActionLogger.php
+++ ActionLogger.php
@@ -258,7 +258,7 @@
 		        $purge_logs_after = get_option( 'al_purge_logs', 0 );
 		        // only purge when it's not set to forever/0
 		        if ( 0 != $purge_logs_after ) {
-			        $now_ts           = current_time( 'timestamp' );
+			        $now_ts           = (int) current_time( 'timestamp' );
 			        $purge_range      = $purge_logs_after * 24 * 60 * 60;
 			        $purge_date       = $now_ts - $purge_range;
 
@@ -269,7 +269,7 @@
                             DELETE FROM {$wpdb->prefix}action_logs
                             WHERE action_time < %d
                             ",
-					        $now_ts
+                            $purge_date
 				        )
 			        );
                 }
