599e4d3655ee82c6379c0f2596145456e9b2b3ae
Beee <info@berryplasman.com>
2018-02-27 12:57:57 +0100

    Fix cron

--- includes/al-crons.php
+++ includes/al-crons.php
@@ -3,7 +3,7 @@
 		$purge_logs_after = get_option( 'al_purge_logs', 0 );
 		// only purge when it's not set to forever/0
 		if ( 0 != $purge_logs_after ) {
-			$now_ts           = current_time( 'timestamp' );
+			$now_ts           = (int) current_time( 'timestamp' );
 			$purge_range      = $purge_logs_after * 24 * 60 * 60;
 			$purge_date       = $now_ts - $purge_range;
 
@@ -11,10 +11,10 @@
 			$wpdb->query(
 				$wpdb->prepare(
 					"
-	                            DELETE FROM {$wpdb->prefix}action_logs
-	                            WHERE action_time < %d
-	                            ",
-					$now_ts
+                    DELETE FROM {$wpdb->prefix}action_logs
+                    WHERE action_time < %d
+                    ",
+                    $purge_date
 				)
 			);
 		}
